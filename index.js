9e582904753286463e26f09fdbc5c33ac41283cb
Nataly <nataly247@gmail.com>
2017-11-26 15:40:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  //string receive a data from prompt ?
+  //string("Hello");
+  //it normalize all letters
+  //it makes all letters caps
+
+  //return string
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log(shout)
+}
+
+function logWhisper(string){
+  console.log(whisper)
+}
+
+shout();
